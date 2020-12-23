7fe7557fa57e84d5d001f40479eec9040ecb83be
Анастасия <anastasianuzdina@gmail.com>
2020-12-23 13:16:33 +0500

    task 4 done (№9)

---
+++ task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = GetValue();
+
+            Console.WriteLine("x = " + x);
+
+            Console.ReadKey();
+        }
+
+        static double GetValue()
+        {
+            return F(3, 2, 3) + F(5, 3, 5) + F(7, 4, 7);
+        }
+        /// <summary>
+        /// Вычисление повторяющегося выражения
+        /// </summary>
+        /// <param name="a">Первое слагаемое числителе</param>
+        /// <param name="b">второе слагаемое в числителе</param>
+        /// <param name="c">степень</param>
+        /// <returns></returns> 
+        static double F(double a, double b, double c)
+        {
+            return Math.Log10(a + Math.Pow(b, c)) / (Math.Log10(b + Math.Pow(c, b)));
+
+        }
+    }
+}
