82c56ce96d6b5bfe72b76d40b1468c64fb697464
Анастасия <anastasianuzdina@gmail.com>
2020-12-23 12:54:54 +0500

    task 3 done (№9)

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathFunction("x"));
+        }
+
+
+        public static double MathFunction(string a)
+        {
+            Console.WriteLine("Введите число " + a);
+            double x = double.Parse(Console.ReadLine());
+            return Math.Abs(1 + Math.Sqrt(x + 1)) / Math.Abs(1 - Math.Sqrt(x - 1));
+        }
+    }
+}
