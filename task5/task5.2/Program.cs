c0b7ff8505955c5e02b916f3fbe28e760a9bc690
Анастасия <anastasianuzdina@gmail.com>
2020-12-23 14:06:12 +0500

    task 5.2 added

---
+++ task5/task5.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace task_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var krater = "кратер";
+            string kater = krater.Substring(0, 1) + krater.Substring(2, 1) + krater.Substring(3, 1) + krater.Substring(4, 1) + krater.Substring(5, 1);
+            string reka = krater.Substring(1, 1) + krater.Substring(4, 1) + krater.Substring(0, 1) + krater.Substring(2, 1);
+            Console.WriteLine($"Результат: {kater}, {reka}.");
+            Console.ReadKey();
+        }
+    }
+}
