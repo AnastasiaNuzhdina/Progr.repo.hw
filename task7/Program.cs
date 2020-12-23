37ed125f08bae6dbb0bae957a2a8d3e26c5c1eba
Анастасия <anastasianuzdina@gmail.com>
2020-12-23 15:49:46 +0500

    task 7 done

---
+++ task7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, f;
+            Console.WriteLine("Введите значение x:");
+            x = double.Parse(Console.ReadLine());
+
+            if (Math.Sin(x) > 0)
+            {
+                f = Math.Cos(x);
+                Console.WriteLine("Значение функции F(x):" + f);
+            }
+            else
+                if (Math.Sin(x) == 0)
+            {
+                f = Math.PI / 2;
+                Console.WriteLine("Значение функции F(x):" + f);
+            }
+            else
+                if (Math.Sin(x) < 0)
+            {
+                f = -Math.Cos(x);
+                Console.WriteLine("Значение функции F(x):" + f);
+            }
+            Console.ReadKey();
+        }
+
+
+
+
+    }
+}
